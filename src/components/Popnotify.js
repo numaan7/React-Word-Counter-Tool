0b1cb700e9f675cca168a3f5a5eb6b6048d6d8d3
numaan7 <101054796+numaan7@users.noreply.github.com>
2024-08-25 23:37:28 -0500

    updates

--- src/components/Popnotify.js
+++ src/components/Popnotify.js
@@ -3,13 +3,13 @@ import React from 'react'
 function Popnotify(props) {
   return (
     <>
-   {  (props.msg) && <div className={`alert alert-${props.msg.type} alert-dismissible fade show  m-2`} role="alert">
-  <strong className='text-uppercase'>{props.msg.type} :</strong> {props.msg.msg}
-  
-</div> }
+      {(props.msg) && <div className={`alert alert-${props.msg.type} alert-dismissible fade show  m-2`} role="alert">
+        <strong className='text-uppercase'>{props.msg.type} :</strong> {props.msg.msg}
+
+      </div>}
     </>
   )
- 
+
 }
 
 export default Popnotify
